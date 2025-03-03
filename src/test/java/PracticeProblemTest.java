8525ba95eed1f22deb8d9a0b06e43d541c73a6a9
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-03-03 18:19:29 -0500

    Update PracticeProblemTest.java

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -1,49 +1,177 @@
+import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import java.io.*;
-
 public class PracticeProblemTest {
+@Test
+@DisplayName("")
+void divideTest3() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = {int.class, int.class};
+        Method method = testClass.getDeclaredMethod("divide", cArg);
+  // Enter code here
+    assertEquals(0, (int)method.invoke(null, 15, 0));
+}
 
-   @Test
-   public void testOutput()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
 
-     // action
-     PracticeProblem.q1();
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void safeConvertStringtoIntTest() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = {String.class};
+        Method method = testClass.getDeclaredMethod("safeConvertStringtoInt", cArg);
+  // Enter code here
+    assertEquals(5, (int)method.invoke(null, "5"));
+}
 
-     // assertion
-     assertEquals("There once was a man from St. Ives.\n", bos.toString());
 
-     // undo the binding in System
-     System.setOut(originalOut);
-   }
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void safeConvertStringtoIntTest2() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = {String.class};
+        Method method = testClass.getDeclaredMethod("safeConvertStringtoInt", cArg);
+  // Enter code here
+    assertEquals(0, (int)method.invoke(null, "5a"));
+}
 
-   @Test
-   public void testInputandOutput()
-   {
-      String data = "Users Input";
-      System.setIn(new ByteArrayInputStream(data.getBytes()));
-      
-      PrintStream originalOut = System.out;
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      System.setOut(new PrintStream(bos));
 
-      // action
-      PracticeProblem.q1();
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void safeConvertStringtoIntTest4() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = {String.class};
+        Method method = testClass.getDeclaredMethod("safeConvertStringtoInt", cArg);
+  // Enter code here
+    assertEquals(0, (int)method.invoke(null, "!"));
+}
 
-      // assertion
-      assertEquals("There once was a man from St. Ives.\n", bos.toString());
 
-      // undo the binding in System
-      System.setOut(originalOut);
-   }
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void divideTest1() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = {int.class, int.class};
+        Method method = testClass.getDeclaredMethod("divide", cArg);
+  // Enter code here
+    assertEquals(2, (int)method.invoke(null, 10, 5));
+}
 
-   @Test
-   public void testQ3()
-   {
-     
-   }
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void divideTest2() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = {int.class, int.class};
+        Method method = testClass.getDeclaredMethod("divide", cArg);
+  // Enter code here
+    assertEquals(2, (int)method.invoke(null, 12, 5));
+}
+
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void validIndexTest2() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = { int[].class, int.class};
+        Method method = testClass.getDeclaredMethod("validIndex", cArg);
+  // Enter code here
+    assertEquals(true, (boolean)method.invoke(null, new int[]{1, 3, 5, 5, 2}, 0));
+}
+
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void validIndexTest1() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = { int[].class, int.class};
+        Method method = testClass.getDeclaredMethod("validIndex", cArg);
+  // Enter code here
+    assertEquals(false, (boolean)method.invoke(null, new int[]{1, 3, 5}, 4));
+}
+
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
+@Test
+@DisplayName("")
+void safeConvertStringtoIntTest3() {
+    Class<?> testClass = PracticeProblem.class;
+    try {
+        Class[] cArg = {String.class};
+        Method method = testClass.getDeclaredMethod("safeConvertStringtoInt", cArg);
+  // Enter code here
+    assertEquals(0, (int)method.invoke(null, ""));
+}
+
+
+    catch (NoSuchMethodException e) {
+        fail("Method does not exist");
+}
+    catch(Exception e) {
+        fail("Something weird happened");
+}
+}
 }
